af828b3aeb6ca265917d9d45ecfc9f8949271b6a
TEMESGEN ABDISSA <109625337+TEMESGENABDISSA@users.noreply.github.com>
2024-02-29 17:19:19 +0300

    Regex integration testing

--- 0x06-unittests_in_js/9-api/api.js
+++ 0x06-unittests_in_js/9-api/api.js
@@ -1,21 +1,17 @@
 const express = require('express');
 
 const app = express();
+const port = 7865;
 
-app.get('/cart', (req, res) => {
-  res.status = 200;
+app.get('/', (_req, res) => {
   res.send('Welcome to the payment system');
 });
 
-app.get('/cart/:id(\\d+)', (req, res) => {
+app.get('/cart/:id([0-9]+)', (req, res) => {
   const id = req.params.id;
-
   res.send(`Payment methods for cart ${id}`);
 });
 
-const PORT = 7865;
-app.listen(PORT, () => {
-  console.log(`API available on localhost port ${PORT}`);
+app.listen(port, () => {
+  console.log(`API available on localhost port ${port}`);
 });
-
-module.exports = app;
